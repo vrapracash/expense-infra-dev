bb44fdf5e647d05076c37c7d2fdc959c54357bee
vrapracash <veeraprakash.bg@gmail.com>
2025-02-20 07:14:05 +0530

    script-update

---
+++ 10-vpc/parameters.tf
@@ -0,0 +1,27 @@
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  type  = "String"
+  overwrite = true
+  value = module.vpc.vpc_id
+}
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  overwrite = true
+  value = join(",", module.vpc.public_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  overwrite = true
+  value = join(",", module.vpc.private_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
+  type  = "StringList"
+  overwrite = true
+  value = join(",", module.vpc.database_subnet_ids)
+}
