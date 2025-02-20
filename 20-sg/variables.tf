a01e0c8172a45f4867dc7900a11e293fedeb4a69
vrapracash <veeraprakash.bg@gmail.com>
2025-02-20 07:48:23 +0530

    script-update

---
+++ 20-sg/variables.tf
@@ -0,0 +1,45 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "mysql_sg_tags" {
+    default = {
+        Component = "mysql"
+    }
+}
+
+variable "backend_sg_tags" {
+    default = {
+        Component = "backend"
+    }
+}
+
+variable "frontend_sg_tags" {
+    default = {
+        Component = "frontend"
+    }
+}
+
+variable "bastion_sg_tags" {
+    default = {
+        Component = "bastion"
+    }
+}
+
+variable "ansible_sg_tags" {
+    default = {
+        Component = "ansible"
+    }
+}
